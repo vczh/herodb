1a722f2dfd7b05fdbdf3b5a755f1850036780ecf
vczh <vczh@163.com>
2014-09-18 21:25:25 +0800

    Log page structure comment

--- Kernel/Source/Utility/Log.cpp
+++ Kernel/Source/Utility/Log.cpp
@@ -1,5 +1,19 @@
 #include "Log.h"
 
+/*
+ *	Page Structure
+ *		Index Page				: [uint64 AddressItems][uint64 NextIndexPage]{[uint64 AddressItem] ...}
+ *			The first index page is the index page from the source.
+ *			Each address item is the address of transactions of 0, 1, ...
+ * 		Log Page				:
+ * 			Transaction Header	: [uint64 Transaction]<Item-Header>
+ * 				Min Size = 5 * sizeof(uint64)
+ * 			Item Header			: [uint64 ItemLength]<Item-Block>
+ * 				Min Size = 4 * sizeof(uint64)
+ * 			Item Block			: [uint64 CurrentBlockLength][uint64 NextBlockAddress/NextItemAddress][data, adjust to sizeof(uint64)]
+ * 				Min Size = 3 * sizeof(uint64)
+ */
+
 namespace vl
 {
 	namespace database
